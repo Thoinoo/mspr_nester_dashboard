6823bb3825d2c06e2a2bd26b52855241c66851c9
Antoine <thoino@outlook.fr>
2025-02-03 19:58:14 +0100

    adding certs for web browser

--- fastapi-dashboard/next.config.js
+++ fastapi-dashboard/next.config.js
@@ -4,8 +4,8 @@ const path = require('path');
 module.exports = {
   devServer: {
     https: {
-      key: fs.readFileSync(path.join(__dirname, '/etc/ssl/nester/cert_api.key')),
-      cert: fs.readFileSync(path.join(__dirname, '/etc/ssl/nester/cert_api.crt')),
+      key: fs.readFileSync(path.join(__dirname, '/certs/cert_api.key')),
+      cert: fs.readFileSync(path.join(__dirname, '/certs/cert_api.crt')),
     },
   },
 };
