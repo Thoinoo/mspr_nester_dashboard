ccef9de3202a539f7bfc448350275d791114ab59
Antoine <thoino@outlook.fr>
2025-02-03 19:42:34 +0100

    adding certs

--- fastapi-dashboard/pages/client-detail/[clientName].js
+++ fastapi-dashboard/pages/client-detail/[clientName].js
@@ -2,6 +2,7 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Link from "next/link";
+import https from "https"; // Import de https
 
 export default function ClientDetail() {
   const { clientName } = useRouter().query;
@@ -16,7 +17,16 @@ export default function ClientDetail() {
 
   const fetchClientDetail = async () => {
     try {
-      const response = await axios.get(`/api/clients/${clientName}`);
+      // Créer un agent HTTPS avec la configuration pour ignorer les erreurs de certificat
+      const agent = new https.Agent({
+        rejectUnauthorized: false, // Ignorer la vérification des certificats SSL
+      });
+
+      // L'URL complète de votre API pour les détails du client
+      const apiUrl = `https://nester.foot.lan:57935/clients/${clientName}`;  // Remplacez par l'URL de votre API
+
+      // Ajouter l'agent HTTPS à la requête axios
+      const response = await axios.get(apiUrl, { httpsAgent: agent });
       setClient(response.data);
     } catch (error) {
       console.error("Error fetching client details:", error);
