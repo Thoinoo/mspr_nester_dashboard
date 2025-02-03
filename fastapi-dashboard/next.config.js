6841f248c278298208468647ee8d2bedcdac3f2b
Antoine <thoino@outlook.fr>
2025-02-03 20:15:12 +0100

    adding certs for web browser

--- fastapi-dashboard/next.config.js
+++ fastapi-dashboard/next.config.js
@@ -1,3 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
 module.exports = {
-  reactStrictMode: true,
+  devServer: {
+    https: {
+      key: fs.readFileSync(path.join(__dirname, './certs/cert_api.key')),
+      cert: fs.readFileSync(path.join(__dirname, './certs/cert_api.crt')),
+    },
+    port: 3000, // Ou 443 si vous voulez utiliser le port HTTPS standard
+  },
 };
