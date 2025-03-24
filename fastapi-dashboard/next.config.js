796bd073d706298a5328264c963dffbf2216fda5
antoine <thoino@outlook.fr>
2025-03-24 19:45:59 +0100

    updated certs

--- fastapi-dashboard/next.config.js
+++ fastapi-dashboard/next.config.js
@@ -4,8 +4,8 @@ const path = require('path');
 module.exports = {
   devServer: {
     https: {
-      key: fs.readFileSync(path.join(__dirname, './certs/cert_api.key')),
-      cert: fs.readFileSync(path.join(__dirname, './certs/cert_api.crt')),
+      key: fs.readFileSync(path.join(__dirname, './certs/cert_nester.key')),
+      cert: fs.readFileSync(path.join(__dirname, './certs/cert_nester.crt')),
     },
     port: 3000, // Ou 443 si vous voulez utiliser le port HTTPS standard
   },
